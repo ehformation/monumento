3d3d26a38dd10241defb8cbb6e4de79e1fe66ff0
eliewebavenue <elie@webavenue.agency>
2025-06-11 12:27:34 +0200

    pipe countrybycolor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
+import { CountryByColorPipe } from './country-by-color.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective
+    BorderCardDirective,
+    CountryByColorPipe
   ],
   imports: [
     BrowserModule,
